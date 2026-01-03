962d4d7e251b52e6d8981959d358d4ef1724dc73
Justshantam <writetoshantam@gmail.com>
2026-01-03 16:51:28 +0530

    nextRound

---
+++ nextRound.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+
+    int a[50];
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+    int check = a[k-1];
+    int count = 0;
+    for(int i=0; i<n; i++){
+        if(a[i] >= check && a[i]>0)
+            count ++;
+    }
+    cout << count;
+    return 0;
+}
