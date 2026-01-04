59c5b20ab06d34594eab3b079dd3959f9d58fbe3
Justshantam <writetoshantam@gmail.com>
2026-01-04 18:23:59 +0530

    122A

---
+++ 122A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a,b;
+    cin >> a >> b;
+    for(int i=0; i<a.size(); i++){
+        a[i] = tolower(a[i]);
+        b[i] = tolower(b[i]);
+        if(a[i]<b[i]){cout << "-1"; return 0;}
+        else if (a[i]>b[i]){cout<<"1"; return 0;}
+    }
+    cout << "0";
+    return 0;
+}
