05807f3f15f3155d8a5e92ef0ef0b39e6423f06d
Justshantam <writetoshantam@gmail.com>
2026-01-11 19:00:06 +0530

    codeforces virtual contest

---
+++ contest/pass.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int k, x;
+        cin >> k >> x;
+        int n = k * x + 1;
+        cout << n << endl;
+    }
+    return 0;
+}
